e78ff80a4eb8afc3e998665aa163ee3ab4a366f9
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-01-04 22:43:55 +0900

    TODOApp: Screen Design - 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/data/todo.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,24 +11,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO APP',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Todo> _todos = [
+    Todo(
+      title: '방 청소',
+      memo: '**시에 방 청소 하기',
+      color: Colors.redAccent.value,
+      done: 0,
+      category: '청소',
+      date: 20220104,
+    ),
+    Todo(
+      title: '방 청소2',
+      memo: '**시에 방 청소 하기2',
+      color: Colors.blue.value,
+      done: 0,
+      category: '청소',
+      date: 20220104,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -36,25 +54,87 @@ class _MyHomePageState extends State<MyHomePage> {
         child: AppBar(),
         preferredSize: const Size.fromHeight(0.0),
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: ListView.builder(
+        itemBuilder: (ctx, idx) {
+          if (idx == 0) {
+            return Container(
+              child: const Text(
+                '오늘하루',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              margin: const EdgeInsets.symmetric(
+                vertical: 12.0,
+                horizontal: 20.0,
+              ),
+            );
+          } else if (idx == 1) {
+            return Container(
+              child: Column(
+                children: List.generate(_todos.length, (_idx) {
+                  Todo t = _todos[_idx];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Color(t.color!),
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12.0,
+                      horizontal: 12.0,
+                    ),
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 20.0,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              t.title!,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Text(
+                              t.done == 0 ? '미완료' : '완료',
+                              style: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          t.memo!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+        itemCount: 4,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.today), label: '오늘'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.today),
-            label: '오늘'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.assignment_outlined),
-              label: '기록'
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.more_horiz),
-              label: '더보기'
-          ),
+              icon: Icon(Icons.assignment_outlined), label: '기록'),
+          BottomNavigationBarItem(icon: Icon(Icons.more_horiz), label: '더보기'),
         ],
       ),
     );
